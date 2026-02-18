90d6fed489e5a92273ab614fa7abf6def3258c22
Kevin Cordes <156715406+Blitzen2@users.noreply.github.com>
2026-02-18 11:25:42 -0800

    Add Two Sum problem solution with hash map
    Implement Two Sum solution using a hash map for efficient lookups.

--- hashmaps/two_sum_hashmap.py
+++ hashmaps/two_sum_hashmap.py
@@ -1,1 +1,14 @@
+"""
+Problem: Two Sum
+Approach: Hash Map (Dictionary)
+Time Complexity: O(n)
+"""
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i]
+            seen[num] = i
